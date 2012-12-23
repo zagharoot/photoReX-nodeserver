9f938e910c1909fc1f7c424f4a725bdaa14b3c26
Ali Nouri <nouri@cs.rutgers.edu>
2012-12-23 10:53:04 -0500

    For #6: what used to happen was that if the available recomm list is empty we would've waited 20 sec to blockingly pop something and then went to check whether we needed to submit new request.

--- src/recommend.js
+++ src/recommend.js
@@ -27,74 +27,79 @@ function recommend(req, res, next){
 		if (redisClient.errorCheck(err, next))
 			return; 
 	}); 		//total number of recommendations (doesn't need to be synced with other commands)
+
 	
 	var qkey = keys.userQueue(req.body.userid); // 'user:' + req.body.userid + ':queue'; 
 
-	//read a blocking from the queue 
-	redisClient.blpop(qkey, 20, function (err, reply){
-		if (redisClient.errorCheck(err, next))
-			return; 
 
-		if (!reply)
-		{
-			var e = require('./serverError').internalError(Error.IN_RECOMMENDATION_TIMEOUT); 
-			next(e); 
-		}else
-		{
-//			console.log(reply[1]); 
-			var recom = JSON.parse(reply[1]); 
-			
-			//we need to add viewed property to the objects 
-			var userViewedKey = keys.userViewed(req.body.userid); 
-			var multi = redisClient.multi(); 
-			for(var i=0; i< recom.pics.length; i++)
-			{
-				multi.zscore(userViewedKey, recom.pics[i].picture.hash); 
-			}
-
-			multi.exec(function(err, replies){
-				
-				//in case of error, we can still send recom (we were unable to augment data)
-				if (err || replies == null || replies.length < recom.pics.length)
-					res.send(reply[1]); 
-				
-				
-				for (var j=0; j< replies.length; j++)
-				{
-//					console.log(recom.pics[j].picture.hash + " : " + replies[j]); 
-					if (replies[j] != null) 
-						recom.pics[j].picture.isViewed = true; 
-					else
-						recom.pics[j].picture.isViewed = false; 
-				}
-
-				res.send(JSON.stringify(recom)); 
-			});
-			
-			
-			//save the current visit in redis (if we don't here, it's not saved in redis!) 
-			if (recom.hasOwnProperty('pageid'))
-			{
-				var val = "" + recom.pageid + ":" + new Date().getTime();
-				redisClient.rpush(keys.userPages(req.body.userid), val); 
-			}
-		}
-		
-	});
-	
-	
-	//we quickly get to here, no matter if queue is empty or not. we can ask for more recommendations in parallel
+	// read the len of the available recommendation queue. if low, we need to submit a new one to the recommendations queue
 	redisClient.llen(qkey, function(err, reply){
 		if (redisClient.errorCheck(err, next))
 			return; 
 
 		var len = parseInt(reply); 
 		
-		if (len< MIN_QUEUE_LENGTH)		//add a recommendation request to the queue of recommendations 
+		if (len< MIN_QUEUE_LENGTH)		//add a recommendation request more 
 		{
 			redisClient.rpush(keys.usersRecommendQueue(), '' + req.body.userid + ':' + (MIN_QUEUE_LENGTH-len+3)); 
 		}		
+
+		//read a blocking from the queue 
+		redisClient.blpop(qkey, 20, function (err, reply){
+			if (redisClient.errorCheck(err, next))
+				return; 
+
+			if (!reply)
+			{
+				var e = require('./serverError').internalError(Error.IN_RECOMMENDATION_TIMEOUT); 
+				next(e); 
+			}else
+			{
+//				console.log(reply[1]); 
+				var recom = JSON.parse(reply[1]); 
+				
+				//we need to add viewed property to the objects 
+				var userViewedKey = keys.userViewed(req.body.userid); 
+				var multi = redisClient.multi(); 
+				for(var i=0; i< recom.pics.length; i++)
+				{
+					multi.zscore(userViewedKey, recom.pics[i].picture.hash); 
+				}
+
+				multi.exec(function(err, replies){
+					
+					//in case of error, we can still send recom (we were unable to augment data)
+					if (err || replies == null || replies.length < recom.pics.length)
+						res.send(reply[1]); 
+					
+					
+					for (var j=0; j< replies.length; j++)
+					{
+//						console.log(recom.pics[j].picture.hash + " : " + replies[j]); 
+						if (replies[j] != null) 
+							recom.pics[j].picture.isViewed = true; 
+						else
+							recom.pics[j].picture.isViewed = false; 
+					}
+
+					res.send(JSON.stringify(recom)); 
+				});
+				
+				
+				//save the current visit in redis (if we don't here, it's not saved in redis!) 
+				if (recom.hasOwnProperty('pageid'))
+				{
+					var val = "" + recom.pageid + ":" + new Date().getTime();
+					redisClient.rpush(keys.userPages(req.body.userid), val); 
+				}
+			}
+			
+		});
 	}); 
+	
+	
+	
+	
 }
 
 
